1718fe8ed5eca992ba4b43b23d2c970a1a572761
Mark Lillicrap <mark.lillicrap@hmcts.net>
2019-08-28 16:15:25 +0100

    Add test 3 for Broadband and mobile

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -62,7 +62,32 @@ describe("filter", () => {
       expect(productTypes.includes("TV")).toBeTruthy();
     });
   });
-  // WHEN filtering by broadband AND mobile THEN show the 1 deal for broadband and mobile only
+
+  it("should return all broadband and mobile deals when broadband and mobile filter are applied", () => {
+    // WHEN filtering by broadband AND mobile THEN show the 1 deal for broadband and mobile only
+    // Arrange
+    const sut = new Store();
+    sut.setDeals(mockData.deals);
+    sut.setProductFilter("Broadband");
+    sut.setProductFilter("Mobile");
+    // Act
+    const results = sut.deals;
+
+    // Assert
+    expect(results.length).toEqual(1);
+    results.forEach(result => {
+      const productTypes = result.productTypes.filter(
+        prodType => prodType !== "Phone"
+      );
+      expect(productTypes.length).toEqual(2);
+      expect(
+        productTypes.includes("Broadband") ||
+          productTypes.includes("Fibre Broadband")
+      ).toBeTruthy();
+      expect(productTypes.includes("Mobile")).toBeTruthy();
+    });
+  });
+
   // WHEN filtering by Sky THEN show the 1 deal for Sky only
   // WHEN filtering by BT, broadband AND tv THEN show the 2 deals for BT with broadband and tv only
 });
